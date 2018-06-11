0957894c484816c3dca042e2246a5d20b1cf039d
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2018-06-11 13:55:16 +0200

    Clang for CI

--- scripts/travis-ci/android_prepare.sh
+++ scripts/travis-ci/android_prepare.sh
@@ -17,8 +17,6 @@ DOCKER_IMG="$DOCKER_REPO:$DOCKER_DIST"
 # Docker Hub.
 get_docker_image
 
-display_debug
-
 # Make shared environment variables.
 make_environement_file
 
@@ -46,8 +44,6 @@ set_repositories_android
 # Install SDK components.
 get_sdk_components
 
-display_debug
-
 # INSTALL/BUILD ROS2 AMENT...
 echo -e "\n\e[33;1mINSTALL/BUILD ROS2 AMENT...\e[0m"
 mkdir -p $HOME_BUILD/ament_ws/src
@@ -55,7 +51,6 @@ cd $HOME_BUILD/ament_ws
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) -w $(pwd) $DOCKER_IMG sh -c "/usr/bin/wget https://gist.githubusercontent.com/Theosakamg/e6084cfafa6b7ea690104424cef970a2/raw/ament_java.repos -nv"
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) -w $(pwd) $DOCKER_IMG sh -c "/usr/bin/vcs import src < ament_java.repos"
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) -w $(pwd) $DOCKER_IMG sh -c "src/ament/ament_tools/scripts/ament.py build --parallel --symlink-install --isolated"
-display_debug
 
 # INSTALL ROS2 WS...
 echo -e "\n\e[33;1mINSTALL ROS2 WS...\e[0m"
@@ -63,7 +58,6 @@ mkdir -p $ROS2WS/src
 cd $ROS2WS
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) -w $(pwd) $DOCKER_IMG sh -c "/usr/bin/wget https://gist.githubusercontent.com/Theosakamg/617cd893813163cdcb9943a08d667964/raw/ros2_java_android.repos -nv"
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) -w $(pwd) $DOCKER_IMG sh -c "/usr/bin/vcs import src < ros2_java_android.repos"
-display_debug
 
 # Patch for Java support.
 echo "Patch for Java support."
@@ -102,7 +96,6 @@ fi
 echo -e "\n\e[33;1mBUILD ROS2 WS...\e[0m"
 cd $HOME_BUILD
 docker run -u "$UID" -it --rm -v $(pwd):$(pwd) --env-file "$HOME_ENV" -w $(pwd) "$DOCKER_IMG" sh -c ". $HOME_BUILD/ament_ws/install_isolated/local_setup.sh && cd $ROS2WS && ament build --parallel --isolated --cmake-args -DPYTHON_EXECUTABLE=$PYTHON_PATH -DTHIRDPARTY=ON -DCMAKE_FIND_ROOT_PATH=\"$ROOT_PATH\" -DANDROID_FUNCTION_LEVEL_LINKING=OFF -DANDROID_TOOLCHAIN_NAME=\"$ANDROID_GCC\" -DANDROID_STL=gnustl_shared -DANDROID_ABI=\"$ANDROID_ABI\" -DANDROID_NDK=\"$ANDROID_NDK_HOME\" -DANDROID_NATIVE_API_LEVEL=\"$ANDROID_VER\" -DCMAKE_TOOLCHAIN_FILE=\"$ROS2JAVA_PATH\" -DANDROID_HOME=\"$ANDROID_SDK_ROOT\" -- --ament-gradle-args -g $HOME_BUILD/.gradle -Pament.android_stl=gnustl_shared -Pament.android_abi=\"$ANDROID_ABI\" -Pament.android_ndk=\"$ANDROID_NDK_HOME\" --stacktrace -- "
-display_debug
 
 echo -e "\n\e[33;1mClean ROS2 build WS...\e[0m"
 rm -rf $HOME_BUILD/ros2_java_ws/build_isolated
